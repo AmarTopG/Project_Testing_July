29abef07fa6fd4ce9701c1f4a7a52828e0df5fde
AmarTopG <amar1k391m@gmail.com>
2023-07-11 22:43:16 +0530

    ProjectK

---
+++ src/test/java/org/test/gitts/Testing_July.java
@@ -0,0 +1,16 @@
+package org.test.gitts;
+
+import io.github.bonigarcia.wdm.GitHubApi;
+
+public class Testing_July {
+	
+	public static void meth() {
+		System.out.println("Move to Git");
+
+	}
+	public static void main(String[] args) {
+		meth();
+		
+	}
+
+}
